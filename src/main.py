7c3fa83e25e2f4b20be173b311c145e87606cae6
cam <cammclain@skiff.com>
2024-07-07 11:48:50 -0500

    add `rich_click` & optimize application flow

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 from __future__ import annotations
-
+import rich_click as click
 from cryptflows.app import create_workflows_app, initialize_application_services
+from datetime import datetime
 #from litestar import Litestar
-# Uncomment the following lines if this script is to be run directly
-if __name__ == "__main__":
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("--scope", "-s",type=click.Path(exists=True), default="example_scope")
+def run(scope: str):
+    
+    start_time = datetime.now()
+    
     initialize_application_services()
-    create_workflows_app("example_scope")
+    create_workflows_app(scope)
+
+    click.echo(f"[green][bold]All tasks in all projects have been completed.[/bold][/green]\n\n\n[green]Started at: {start_time}\n[/green][green]Finished at: {datetime.now()}[/green]")
+
+
+def main():
+    cli()
+
+if __name__ == "__main__":
+    main()
