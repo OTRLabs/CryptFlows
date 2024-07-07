04b3dfef108ad48331ce3f2e3a9c9770e11b01f3
cam <cammclain@skiff.com>
2024-07-07 17:42:18 -0500

    add list models functions

--- src/cryptflows/services/artificial_intelligence/large_language_models/ollama/ollama_utils.py
+++ src/cryptflows/services/artificial_intelligence/large_language_models/ollama/ollama_utils.py
@@ -3,3 +3,15 @@ from __future__ import annotations
 from .....configs.config import Config
 
 import ollama
+
+
+def init_ollama():
+    pass
+
+
+def get_list_of_models_from_ollama() -> list:
+    
+    list_of_models: list = ollama.list()
+    
+    return list_of_models
+    
