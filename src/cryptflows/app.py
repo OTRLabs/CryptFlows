7b0c67f9d11da0eef9acb588b093aa5ec123d6d6
cam <cammclain@skiff.com>
2024-07-07 11:42:41 -0500

    clean up & optimize the service init

--- src/cryptflows/app.py
+++ src/cryptflows/app.py
@@ -2,39 +2,39 @@ from __future__ import annotations
 from rich import print
 import asyncio
 import logging
+
 from .configs.config import Config
 from .workflows.project_management.project_utils import init_project_db_session
 from .workflows.tasking.tasking_utils import send_task, consume_task
 
-
 def initialize_application_services() -> None:
+    """
+    Initializes the application services.
+
+    This function initializes the project database session and prints a message indicating that projects
+    can be created in the database. It also prints a message indicating that tasks to be completed from
+    a previous run need to be implemented.
+
+    :return: None
+    """
     print("[green]Initializing project database session...[/green]")
     init_project_db_session()
-    print(f"[green]Project database session initialized\n\nProject's can be created in the database[/green]")
+    print("[green]Project database session initialized. Projects can be created in the database.[/green]")
 
-    # TODO: Implement checking to see if there are any existing tasks in any projects that need to be completed from a previous run
-
+    # TODO: Implement checking for existing tasks that need completion from a previous run
     print("[green]Initializing workflows application...[/green]")
 
-    # connect to the main database
-
     
-def create_workflows_app(scope: str) -> None:
 
+def create_workflows_app(scope: str) -> None:
     print("[green]Initializing application...[/green]")
+    
     # Configure logging
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
-    
     print(f"[green]Logger level: {logging.getLevelName(logger.level)}[/green]")
-    
-    
 
-    print(f"[green]Initializing workflows application[/green]")
     print("[green]Creating workflows application...[/green]")
-    
-
-
     
     # Example task and host
     example_task = {
@@ -46,3 +46,5 @@ def create_workflows_app(scope: str) -> None:
     # Send and consume tasks
     asyncio.run(send_task(example_task, example_host))
     asyncio.run(consume_task(example_task, example_host))
+
+
