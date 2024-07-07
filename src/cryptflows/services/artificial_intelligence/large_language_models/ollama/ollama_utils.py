097c729255e8ce92b472bfe08382e9a914fcfe1e
cam <cammclain@skiff.com>
2024-07-07 17:59:16 -0500

    adding more functionality to ollama. adding async function that passes prompt to specified models

--- src/cryptflows/services/artificial_intelligence/large_language_models/ollama/ollama_utils.py
+++ src/cryptflows/services/artificial_intelligence/large_language_models/ollama/ollama_utils.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from .....configs.config import Config
-
+from rich import Console
+from rich.columns import Columns
 import ollama
 
 
@@ -9,9 +10,12 @@ def init_ollama():
     pass
 
 
-def get_list_of_models_from_ollama() -> list:
+def get_list_of_models_from_ollama(console: Console, config: Config) -> list:
     
     list_of_models: list = ollama.list()
-    
+    console.print("[bold green]Available models:[/bold green]")
+    console.print(Columns(list_of_models), style="green")
     return list_of_models
-    
+
+async def prompt_an_ollama_model(console: Console, config: Config) -> str:
+    pass
