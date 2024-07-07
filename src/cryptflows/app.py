c3c22822d6536855d0f81dfb8949f57bbc85bed6
cam <cammclain@skiff.com>
2024-07-07 10:45:18 -0500

    adding packages to project venv

--- src/cryptflows/app.py
+++ src/cryptflows/app.py
@@ -1,16 +1,22 @@
 from __future__ import annotations
 from rich import print
 import asyncio
-
+import logging
 from .configs.config import Config
 
 from .workflows.tasking.tasking_utils import send_task, consume_task
 
 def create_workflows_app(scope: str) -> None:
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    
+    print(f"[green]Logger level: {logging.getLevelName(logger.level)}[/green]")
     
     print("[green]Creating workflows application...[/green]")
     
-    
+
+
     
     # Example task and host
     example_task = {
