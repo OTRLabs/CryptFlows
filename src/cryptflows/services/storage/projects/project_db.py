382a04963eb1b09b9ca5983f67919b93c2d01764
Cam <cammclain@skiff.com>
2024-07-08 23:50:30 -0500

    adding .env var for config for sqlite db path. not hardcoded

--- src/cryptflows/services/storage/projects/project_db.py
+++ src/cryptflows/services/storage/projects/project_db.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine, Column, Integer, String, Enum, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-
+from ....configs.config import Config
 Base = declarative_base()
-engine = create_engine('sqlite:///cybersecurity_projects.db', echo=True)
+engine = create_engine(f"{Config.SQLITE_DB_PATH}", echo=True)
 
 class Project(Base):
     __tablename__ = 'projects'
