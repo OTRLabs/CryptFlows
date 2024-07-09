b908c2cd199ababacd354a3573b2337ce846d6e2
Cam <cammclain@skiff.com>
2024-07-09 00:53:15 -0500

    remove second instance of "Project" model

--- src/cryptflows/services/storage/projects/project_db.py
+++ src/cryptflows/services/storage/projects/project_db.py
@@ -3,18 +3,5 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from ....configs.config import Config
-Base = declarative_base()
-engine = create_engine(f"{Config.SQLITE_DB_PATH}", echo=True)
-
-class Project(Base):
-    __tablename__ = 'projects'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    project_type = Column(Enum('Hack the Box', 'Bug Bounty', 'Red Team', 'Penetration Test', 'Security Compliance', name='project_types'), nullable=False)
-    start_date = Column(DateTime, default=datetime.utcnow)
-    scope = Column(String)
-    end_date = Column(DateTime)
-    status = Column(Enum('Planned', 'In Progress', 'Completed', name='project_statuses'), default='Planned')
-    description = Column(String)
-
+#Base = declarative_base()
+#engine = create_engine(f"{Config.SQLITE_DB_PATH}", echo=True)
