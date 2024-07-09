cb6b4bfe4b4afb2156e08a95730a2691cc3c4ff6
Cam <cammclain@skiff.com>
2024-07-09 11:34:56 -0500

    adding use of DatabaseUtils

--- src/cryptflows/services/storage/database/db.py
+++ src/cryptflows/services/storage/database/db.py
@@ -4,26 +4,27 @@ from rich import get_console
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Mapped, Session, sessionmaker
 from ....configs.config import Config
-from advanced_alchemy.base import UUIDBase
-from advanced_alchemy.filters import LimitOffset
-from advanced_alchemy.repository import SQLAlchemySyncRepository
-from advanced_alchemy.utils.fixtures import open_fixture
+from rich.console import Console
 
 from .models.models import *
 
 
 from sqlalchemy.engine import Engine
+class DatabaseUtils:
 
-def connect_to_database() -> Engine:
-    """
-    This function connects to the database and returns the database engine.
+    def __init__(self, console: Console) -> None:
+        console.print("Initializing database...", style="bold green")
 
-    Returns:
-        Engine: The SQLAlchemy database engine.
-    """
-    duck_db_engine: Engine = create_engine(
-        f"duckdb:///{Config.DUCKDB_PATH}",
-    )
-    return duck_db_engine
+    def connect_to_database() -> Engine:
+        """
+        This function connects to the database and returns the database engine.
 
-    
+        Returns:
+            Engine: The SQLAlchemy database engine.
+        """
+        duck_db_engine: Engine = create_engine(
+            f"duckdb:///{Config.DUCKDB_PATH}",
+        )
+        return duck_db_engine
+
+        
