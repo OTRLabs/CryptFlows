1335b7066f2597f5980765a222bc39b994f33864
Cam <cammclain@skiff.com>
2024-07-09 02:02:58 -0500

    add "init" & self.XYZ to ScopeUtils class

--- src/cryptflows/services/workflows/project_management/scope_utils.py
+++ src/cryptflows/services/workflows/project_management/scope_utils.py
@@ -4,24 +4,28 @@ import os
 from rich.console import Console
 from rich.prompt import Prompt
 class ScopeUtils:
-    def ask_user_for_scope(console: Console) -> str:
+
+    def __init__(self):
+        pass
+
+    def ask_user_for_scope(self, console: Console) -> str:
         scope: str = Prompt.ask(
             "Enter the path to the scope directory",
         )
 
         return scope
 
-    def scope_exists(console: Console) -> bool:
-        scope = ask_user_for_scope(console)
+    def scope_exists(self, console: Console) -> bool:
+        scope = self.ask_user_for_scope(console)
         if os.path.exists(scope):
             return True
         else:
             return False
 
-    def set_scope(scope: str) -> None:
+    def set_scope(self, console: Console, scope: str) -> None:
         os.environ["SCOPE"] = scope
         console = Console()
         console.print(f"Scope set to: {scope}")
 
-    def get_current_scope(scope: str) -> str:
+    def get_current_scope(self, scope: str) -> str:
         return scope
