0436ed079261ce7888de5370b91e08984bc092cc
Cam <cammclain@skiff.com>
2024-07-11 04:56:10 -0500

    syncing

---
+++ src/cryptflows/domain/accounts/repositories.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from advanced_alchemy.repository import SQLAlchemyAsyncRepository, SQLAlchemyAsyncSlugRepository
+
+from cryptflows.db.models.role import Role
+from cryptflows.db.models.user import User
+from cryptflows.db.models.user_role import UserRole
+
+class UserRepository(SQLAlchemyAsyncRepository[User]):
+    """User SQLAlchemy Repository."""
+
+    model_type = User
+
+
+class RoleRepository(SQLAlchemyAsyncSlugRepository[Role]):
+    """User SQLAlchemy Repository."""
+
+    model_type = Role
+
+
+class UserRoleRepository(SQLAlchemyAsyncRepository[UserRole]):
+    """User Role SQLAlchemy Repository."""
+
+    model_type = UserRole
