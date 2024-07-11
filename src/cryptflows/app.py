2a412cd87fbd299c715a2429df7984967155c2e0
Cam <cammclain@skiff.com>
2024-07-11 02:16:35 -0500

    add get_settings function

--- src/cryptflows/app.py
+++ src/cryptflows/app.py
@@ -1,5 +1,30 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from litestar import Litestar
 
 
 
-def create_app():
-    pass
+def create_app() -> Litestar:
+    """Create ASGI application."""
+
+    from litestar import Litestar
+    from litestar.di import Provide
+
+    #from app.config import app as config
+    #from app.config import constants
+    from app.config.base import get_settings
+    #from app.domain.accounts import signals as account_signals
+    #from app.domain.accounts.dependencies import provide_user
+    #from app.domain.accounts.guards import auth
+    #from app.domain.teams import signals as team_signals
+    #from app.lib.dependencies import create_collection_dependencies
+    #from app.server import openapi, plugins, routers
+
+    #dependencies = {constants.USER_DEPENDENCY_KEY: Provide(provide_user)}
+    #dependencies.update(create_collection_dependencies())
+    settings = get_settings()
+
+    return Litestar(
--- src/cryptflows/config/base.py
+++ src/cryptflows/config/base.py
@@ -2,3 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+
+
+def get_settings() -> None:
+    pass
