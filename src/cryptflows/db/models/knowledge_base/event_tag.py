a7124e0f7f5b7d776464e0716d6526e3c09b28ee
Cam <cammclain@skiff.com>
2024-07-12 16:57:03 -0500

    init event in knowledge base

---
+++ src/cryptflows/db/models/knowledge_base/event_tag.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from sqlalchemy import Table, Column, ForeignKey
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from ...database import KnowledgeBaseBase
+from .event_tag import EventTag, _event_tag
+
+
+_event_tag = Table(
+    'event_tag',
+    KnowledgeBaseBase.metadata,
+    Column('event_id', ForeignKey('event.id'), primary_key=True),
+    Column('tag_id', ForeignKey('tag.id'), primary_key=True)
+)
+
+
+class DocumentTag(KnowledgeBaseBase):
+    __tablename__ = "event_tag"
+    event_id = Column(ForeignKey("event.id"), primary_key=True)
+    tag_id = Column(ForeignKey("tag.id"), primary_key=True)
