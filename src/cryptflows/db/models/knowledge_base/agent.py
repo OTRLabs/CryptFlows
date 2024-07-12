cb4ab69b75bd13c15c47831fa9dce4484e20f0a6
cam <cammclain@skiff.com>
2024-07-12 18:23:23 -0500

    removed unused imports

--- src/cryptflows/db/models/knowledge_base/agent.py
+++ src/cryptflows/db/models/knowledge_base/agent.py
@@ -4,9 +4,7 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from advanced_alchemy.base import UUIDAuditBase
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ...database import KnowledgeBaseBase
