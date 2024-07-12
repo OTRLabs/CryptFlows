a4ee466afe7e167644afb0a4fb8be3bc5a97dc1b
Cam <cammclain@skiff.com>
2024-07-12 14:42:55 -0500

    making basic changes

--- src/cryptflows/db/database.py
+++ src/cryptflows/db/database.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
 
+
 from typing import TYPE_CHECKING
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+from litestar import Litestar, post
+from litestar.contrib.sqlalchemy.plugins import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
+
+if TYPE_CHECKING:
+
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class Base(DeclarativeBase):
+    pass
