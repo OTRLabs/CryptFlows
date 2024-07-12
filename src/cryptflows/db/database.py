58ac209d97105bd8373f954d73ce845abe20b6e4
Cam <cammclain@skiff.com>
2024-07-12 13:56:48 -0500

    init base of database. probably going to remove this

---
+++ src/cryptflows/db/database.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
